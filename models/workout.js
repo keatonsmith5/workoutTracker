05dda2ddedb30aee89b461ac0addc54fd717c1c8
Keaton Smith <keatonpsmith55@gmail.com>
2020-04-30 22:53:49 -0700

    edit workout model

--- models/workout.js
+++ models/workout.js
@@ -45,9 +45,8 @@ const workoutSchema = new Schema({
     }
   });
 
-// adds a dynamically-created property to schema
+
 workoutSchema.virtual("totalDuration").get(function () {
-  // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
