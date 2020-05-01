9ee25e56225eced89a8751bc9c8518e65cc8986c
Keaton Smith <keatonpsmith55@gmail.com>
2020-04-30 22:57:34 -0700

    server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 
 // TODO: import mongoose
+const mongoose = require("mongoose")
 
 const apiRoutes = require("./routes/api");
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.static("public", { "extensions": "html" }));
 
 // TODO: create mongodb connection with mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.use(apiRoutes);
 
