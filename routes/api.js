f6c9fd8c1029abcea33367842f3adf8ad18b0bc6
Keaton Smith <keatonpsmith55@gmail.com>
2020-04-30 22:51:28 -0700

    workout model and api routes done

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,72 @@
 const router = require("express").Router();
 
 // TODO: import required model/s
+conts db = require("../models");
 
 // TODO: and add code to the routes so that the app functions correctly
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
   // CODE HERE
+  db.Workout.create({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
   // CODE HERE
+  db.Workout.findByIdAndUpdate(
+    {_id: req.params.id},
+    { $push: { exercises: req.body } },
+  ).then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
   // CODE HERE
+  db.Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
   // CODE HERE
+  db.Workout.find({}).limit(7)
+  .then(dbWorkouts => {
+    console.log(dbWorkouts)
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 // Delete workout with id matching id in the request body.
 router.delete("/api/workouts", (req, res) => {
   // CODE HERE
+  db.Workout.findByIdAndRemove(req.body.id)
+  .then(() => {
+    res.json(true);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
